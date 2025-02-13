bf4adaa7cb6f6981da9277e395b810f18d469fb5
ClientedeSucesso <contato@clientedesucesso.com.br>
2025-02-13 01:44:13 +0000

    Corrigindo script start e adicionando index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
- */
+const express = require('express');
+const bodyParser = require('body-parser');
 
-module.exports = {
-  log: require('./lib/log'),
-  Wit: require('./lib/wit'),
-  interactive: require('./lib/interactive')
-};
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para processar JSON
+app.use(bodyParser.json());
+
+// Rota de teste
+app.get('/', (req, res) => {
+    res.send('🚀 Servidor rodando com sucesso no Render e no Codespaces!');
+});
+
+// Inicia o servidor na porta correta (necessário para o Render)
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
